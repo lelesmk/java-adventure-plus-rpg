e93b59682e69ad392b57d3203ff41f7dd28bd825
Lele Mkefa <l.mkefa@gmail.com>
2019-12-08 17:36:44 +0200

    Create Player class

---
+++ src/systemPackage/Player.java
@@ -0,0 +1,12 @@
+package systemPackage;
+
+import assetsPackage.SuperWeapon;
+
+public class Player {
+	
+	public int hp;
+	/*first create SuperWeapon super class then import here */
+	public SuperWeapon currentWeapon;
+	
+
+}
